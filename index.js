3db9dad248dc6909d888a7b51beec30d407ecf39
BoaTenG0 <gboateng.ac@st.vvu.edu.gh>
2024-06-08 19:51:31 +0000

    fourth commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: "*",
+    origin: "https://zamper-frontend.netlify.app/",
     methods: ["POST", "GET", "PUT"],
     credentials: true,
     exposedHeaders: ["set-cookie"],
