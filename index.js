ea60cf9e9243f6dd9f10a2c4e0d05b1305609e1e
BoaTenG0 <gboateng.ac@st.vvu.edu.gh>
2024-06-08 14:18:44 +0000

    second commit

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: ["*"],
+    origin: "*",
     methods: ["POST", "GET", "PUT"],
     credentials: true,
     exposedHeaders: ["set-cookie"],
